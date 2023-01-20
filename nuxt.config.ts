ed876734152b695961cc4d86a8f024bf9aaf2b3d
Rock070 <a0938130702@gmail.com>
2023-01-20 21:53:55 +0800

    chore: eslint, husky, lint-staged, unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
+import { defineNuxtConfig } from 'nuxt/config'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    '@nuxt/content',
+    '@unocss/nuxt',
+  ],
+  ssr: false,
+  css: [
+    '@unocss/reset/antfu.css',
+    '~/styles/themes.css',
+  ],
+  // content: {
+  // https://content.nuxtjs.org/api/configuration
+  // }
 })
