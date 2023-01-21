8021c4256796a5eec167c8d8c7998ef340498eea
Rock070 <a0938130702@gmail.com>
2023-01-22 00:59:35 +0800

    fix(post)：重複出現兩次 h1

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -5,11 +5,21 @@ import useQueryPathGetPublishedPost from '~/service/useQueryPathGetPublishedPost
 const route = useRoute()
 
 const { data: article } = useQueryPathGetPublishedPost(route.path)
+
+const display = computed(() =>
+  ({
+    ...article.value,
+    body: {
+      ...article.value?.body,
+      children: article.value?.body.children.filter(node => node.tag !== 'h1') ?? [],
+    },
+  }),
+)
 </script>
 
 <template>
-  <div class="inline-block mb-10">
-    <h1 class="font-bold text-4xl">
+  <div class="inline-block mb-6 lg:mb-10">
+    <h1 class="font-bold text-2xl lg:text-4xl">
       {{ article?.title }}
     </h1>
     <span class="flex justify-center items-center space-x-2 text-sm opacity-60">
@@ -18,7 +28,7 @@ const { data: article } = useQueryPathGetPublishedPost(route.path)
       <span class="whitespace-nowrap">{{ article?.durations }} min read</span>
     </span>
   </div>
-  <article v-if="article">
-    <ContentRenderer class="prose min-w-80vw lg:min-w-70vw break-all" :value="article" />
+  <article v-if="article" class="prose min-w-80vw lg:min-w-70vw break-all">
+    <ContentRenderer :value="display" />
   </article>
 </template>
