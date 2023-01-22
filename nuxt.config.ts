1b276b8287d5882c8cf565242df5557d37bf70ba
Rock070 <a0938130702@gmail.com>
2023-01-22 16:01:32 +0800

    feat(theme): add Docus theme

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  extends: '@nuxt-themes/docus',
   modules: [
     '@nuxt/content',
     '@unocss/nuxt',
