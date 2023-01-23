2d542523995257e18536934cc77db62c3d187652
Rock070 <a0938130702@gmail.com>
2023-01-23 11:56:38 +0800

    feat(404): add 404 page

--- pages/posts/[...slug].vue
+++ pages/posts/[...slug].vue
@@ -2,6 +2,8 @@
 import { useRoute } from 'vue-router'
 
 import useQueryPathGetPublishedPost from '~/service/useQueryPathGetPublishedPost'
+import DocumentDrivenNotFound from '~/components/DocumentDrivenNotFound.vue'
+
 const route = useRoute()
 
 const { data: article } = useQueryPathGetPublishedPost(route.path)
@@ -18,17 +20,18 @@ const display = computed(() =>
 </script>
 
 <template>
-  <div class="inline-block mb-6 lg:mb-10">
-    <h1 class="font-bold text-2xl lg:text-4xl">
+  <div v-if="article" class="inline-block mb-6 lg:mb-10">
+    <h1 class="font-bold text-2xl lg:text-4xl text-center">
       {{ article?.title }}
     </h1>
     <span class="flex justify-center items-center space-x-2 text-sm opacity-60">
-      <time class="whitespace-nowrap min-w-70px"> {{ article?.date }} </time>
+      <time :datetime="article.date" class="whitespace-nowrap min-w-70px"> {{ article?.date_format }} </time>
       <span>-</span>
       <span class="whitespace-nowrap">{{ article?.durations }} min read</span>
     </span>
+    <article class="prose min-w-80vw lg:min-w-70vw break-all">
+      <ContentRenderer :value="display" />
+    </article>
   </div>
-  <article v-if="article" class="prose min-w-80vw lg:min-w-70vw break-all">
-    <ContentRenderer :value="display" />
-  </article>
+  <DocumentDrivenNotFound v-else />
 </template>
