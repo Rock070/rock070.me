5727d81e3f79036d503cfbe389b42a9899d4fe69
Rock070 <a0938130702@gmail.com>
2023-01-24 23:14:48 +0800

    refactor(type): make MDC type more flexible

--- types/query.ts
+++ types/query.ts
@@ -2,7 +2,7 @@ import type { MarkdownParsedContent } from '@nuxt/content/dist/runtime/types'
 
 export interface MyCustomParsedContent extends MarkdownParsedContent {
   categories: string | string[]
-  date: string
-  date_iso_string: string
+  date?: string
+  date_iso_string?: string
   date_format?: string
 }
