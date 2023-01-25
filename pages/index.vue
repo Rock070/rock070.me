38d25662989232e11a63b79d8287d6671ff3467c
Rock070 <a0938130702@gmail.com>
2023-01-25 21:48:06 +0800

    feat(home): add avatar

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <ContentDoc v-slot="{ doc }">
-    <h1>{{ doc.title }}</h1>
+    <div class="text-center mb-10">
+      <img :width="345 * 0.5" :height="331 * 0.5" src="~/public/favicon.png" alt="Rock070 Avatar" class="mb-2">
+      <h1>{{ doc.title }}</h1>
+    </div>
     <article>
       <ContentRenderer :value="doc" />
     </article>
