8768d67408722518754778500a36aed497b91b53
Rock070 <a0938130702@gmail.com>
2023-01-25 23:35:49 +0800

    fix(bio): avatar 引用路徑

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <ContentDoc v-slot="{ doc }">
     <div class="text-center mb-10">
-      <img :width="345 * 0.5" :height="331 * 0.5" src="~/public/favicon.png" alt="Rock070 Avatar" class="mb-2">
+      <img :width="345 * 0.5" :height="331 * 0.5" src="/favicon.png" alt="Rock070 Avatar" class="mb-2">
       <h1>{{ doc.title }}</h1>
     </div>
     <article>
