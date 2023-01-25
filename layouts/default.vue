055b4b6f2d81f48427ef53c916841de81bcfadea
Rock070 <a0938130702@gmail.com>
2023-01-25 23:20:27 +0800

    fix(site): 簡單修正黑夜模式

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@ import MolHeader from '~~/components/Molecules/MolHeader.vue'
 </script>
 
 <template>
-  <div class="min-h-100vh dark:bg-black dark:text-white">
+  <div class="min-h-100vh bg-white text-[#333333] dark:bg-black dark:text-white">
     <MolHeader class="px-4 py-3 lg:p-8" />
     <main class="px-5 pt-5 pb-16 lg:px-8 lg:pt-5 lg:pb-20 flex flex-col items-center">
       <NuxtPage />
