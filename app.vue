aa5b1a525a236f68512939a14753c209103932f4
Rock070 <a0938130702@gmail.com>
2023-02-01 13:50:10 +0800

    chore: remove useless

--- app.vue
+++ app.vue
@@ -1,13 +1,3 @@
-<script setup lang="ts">
-import { useRoute } from 'vue-router'
-import { computed } from 'vue'
-const { meta } = useRoute()
-
-const layoutName = computed(() =>
-  meta?.layout || 'default',
-)
-</script>
-
 <template>
   <div>
     <NuxtPage />
