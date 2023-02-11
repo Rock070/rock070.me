7020c5d51ae9d1c25a68c7ccaac5ac2a90e580ec
Rock070 <a0938130702@gmail.com>
2023-02-11 17:11:25 +0800

    feat: toc

--- layouts/post.vue
+++ layouts/post.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
+import { breakpointsTailwind, isClient, useBreakpoints } from '@vueuse/core'
 import MolHeader from '~/components/Molecules/MolHeader.vue'
 import { useContent, useRuntimeConfig } from '#imports'
 import getReadingTime from '~/helpers/getReadingTime'
 
 import dateFormatter from '~/utils/dateFormatter'
 
-const content = await useContent()
+const breakpoints = useBreakpoints(breakpointsTailwind)
+const isPc = breakpoints.isGreaterOrEqual('lg')
 const config = useRuntimeConfig()
-
+const content = await useContent()
 const { page } = content
 
 const date_format = computed(() => {
@@ -44,24 +46,57 @@ useContentHead(page)
 </script>
 
 <template>
-  <div class="min-h-100vh bg-white text-[#333333] dark:bg-black dark:text-white">
-    <MolHeader class="px-4 py-3 lg:p-8" />
-    <main class="px-5 pt-5 pb-16 lg:px-8 lg:pt-5 lg:pb-20 flex flex-col items-center">
-      <article>
-        <div class="inline-block mb-6 lg:mb-10">
-          <h1 class="font-bold text-2xl lg:text-4xl text-center">
-            {{ page?.title }}
-          </h1>
-          <span class="flex justify-center items-center space-x-2 text-sm opacity-60">
-            <time :datetime="page?.date" class="whitespace-nowrap min-w-70px"> {{ date_format }} </time>
-            <span>-</span>
-            <span class="whitespace-nowrap">{{ durations }} min read</span>
-          </span>
-          <div class="prose min-w-80vw lg:min-w-70vw break-all">
-            <slot />
+  <div class="min-h-100vh bg-white text-[#333333] dark:bg-dark dark:text-white">
+    <MolHeader />
+
+    <div
+      class="grid grid-cols-[20%_55%_20%] relative"
+    >
+      <div />
+      <main class="px-5 pt-5 pb-16 lg:px-8 lg:pt-5 lg:pb-20 items-center">
+        <article class="">
+          <div class="inline-block mb-6 lg:mb-10">
+            <h1 class="font-bold text-2xl lg:text-4xl text-center">
+              {{ page?.title }}
+            </h1>
+            <span class="flex justify-center items-center space-x-2 text-sm opacity-60">
+              <time :datetime="page?.date" class="whitespace-nowrap min-w-70px"> {{ date_format }} </time>
+              <span>-</span>
+              <span class="whitespace-nowrap">{{ durations }} min read</span>
+            </span>
+            <div class="prose">
+              <slot />
+            </div>
           </div>
-        </div>
-      </article>
-    </main>
+        </article>
+      </main>
+      <aside
+        v-if="!isClient || isPc"
+      >
+        <DocsToc
+          class="
+            post-doc
+            sticky top-120px right-0
+            overflow-y-auto
+          "
+        />
+      </aside>
+    </div>
   </div>
 </template>
+
+<style>
+.post-doc {
+  @apply py-10 px-4 w-full;
+}
+
+.post-doc::before {
+  @apply w-px h-[calc(100%-5rem)];
+  @apply content-[''];
+  @apply px-[0.5px] py-10;
+  @apply absolute top-50% left-0;
+  @apply -translate-y-1/2;
+  @apply bg-dark bg-opacity-20;
+  @apply dark:bg-white dark:bg-opacity-50;
+}
+</style>
