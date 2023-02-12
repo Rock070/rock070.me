c5cf574f73a9bfbeb9f5c5af0515ee21aeebcb4d
Rock070 <a0938130702@gmail.com>
2023-02-12 10:24:35 +0800

    feat: remove themes/element

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import svgLoader from 'vite-svg-loader'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  extends: ['@nuxt-themes/docus', '@nuxt-themes/elements'],
+  extends: ['@nuxt-themes/docus'],
   nitro: {
     prerender: {
       routes: ['/sitemap.xml', '/robots.txt'],
